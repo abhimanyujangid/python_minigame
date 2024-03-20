68993e1535341609ca4a5523b510f4319d7536fa
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2024-03-21 01:25:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from itertools import cycle
+from random import randrange
+from tkinter import Canvas, Tk, messagebox, font
+
+canvas_width = 800
+canvas_height = 400
+
+root = Tk()
+c = Canvas(root, width=canvas_width, height=canvas_height, background="deep sky blue")
+c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="sea green", width=0)
+c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
+c.pack()
+
+color_cycle = cycle(["light blue", "light green", "light pink", "light yellow", "light cyan"])
+egg_width = 45
+egg_height = 55
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+difficulty = 0.95
+catcher_color = "blue"
+catcher_width = 100
+catcher_height = 100
+catcher_startx = canvas_width / 2 - catcher_width / 2
+catcher_starty = canvas_height - catcher_height - 20
+catcher_startx2 = catcher_startx + catcher_width
+catcher_starty2 = catcher_starty + catcher_height
+
+catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
+game_font = font.nametofont("TkFixedFont")
+game_font.config(size=18)
